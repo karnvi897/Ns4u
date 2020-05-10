171516da2dc94cde9ecd2ab4af0cbd25590734b1
karnvi897 <64742909+karnvi897@users.noreply.github.com>
2020-05-11 02:02:59 +0530

    a

--- app/src/main/java/com/navneet/ns4u/contacts.java
+++ app/src/main/java/com/navneet/ns4u/contacts.java
@@ -2,17 +2,33 @@ package com.navneet.ns4u;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class contacts extends Fragment {
+
+    private FloatingActionButton addfab;
+    private LinearLayout addcontactlayout;
+    private EditText addname;
+    private EditText addmobile;
+    private Button addcontact;
+
+
+
 
     public contacts() {
         // Required empty public constructor
@@ -24,5 +40,31 @@ public class contacts extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addfab = view.findViewById(R.id.addfab);
+        addcontactlayout = view.findViewById(R.id.addcontactlayout);
+        addname = view.findViewById(R.id.addname);
+        addmobile = view.findViewById(R.id.addmobile);
+        addcontact = view.findViewById(R.id.addcontact);
+
+        addcontactlayout.setVisibility(View.GONE);
+
+        addfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addcontactlayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        addcontact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addcontactlayout.setVisibility(View.GONE);
+            }
+        });
     }
 }
